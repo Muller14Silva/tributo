f641a59c5e6cbcfe77eb94fc753562c79f2dad2d
Muller Silva <muller14silva@gmail.com>
2022-01-03 19:34:51 -0300

    atualizações na responsividade

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu () {
+
+	var menu = document.getElementById("menu");
+
+	if (menu.style.display == 'none') {
+		menu.style.display = "block";
+	}else {
+		menu.style.display = "none";
+	}
+}
